e378ba4b0b13960f5b06e808431eb1eeae27f10c
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-02 19:46:34 +0100

    Print an image of the animal

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
 public class Main {
 
-    public static void main(String[] args) {
 
-        System.out.println("I love animals!");
-        System.out.println("Let's check on the animals...");
-        System.out.println("The deer looks fine.");
-        System.out.println("The bat looks happy.");
-        System.out.println("The lion looks healthy.");
+        public static void main(String[] args) {
+            String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+
+            // write your code here
+            System.out.println(camel);
+        }
     }
-}
+
